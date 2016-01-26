c45ecb069e8908c00bf8faaa891cf242ff6dbd63
TheDudeFromCI <linktheblademaster@yahoo.com>
2016-01-26 11:33:56 -0700

    Added layout for world screen tool bar. Signed-off-by:TheDudeFromCI <linktheblademaster@yahoo.com>

---
+++ src/build/games/wraithaven/WorldScreenToolbar.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package build.games.wraithaven;
+
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author TheDudeFromCI
+ */
+public class WorldScreenToolbar extends JPanel {
+
+    public WorldScreenToolbar() {
+        setPreferredSize(new Dimension(20, 20));
+    }
+}
